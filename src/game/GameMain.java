118a37b464e924365b99e72141f8210f0122f273
ben7238 <39558903+ben7238@users.noreply.github.com>
2018-05-30 09:20:54 -0400

    jlk

--- src/game/GameMain.java
+++ src/game/GameMain.java
@@ -1,10 +1,17 @@
 package game;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 
-public class GameMain {
-
+public class GameMain extends JFrame implements ActionListener {
+	
+	private static JButton hit   = new JButton("Hit Me");
+	private static JButton stand = new JButton("Stand");
+	private static JButton doubleDown  = new JButton("Double Down");
+	private static JButton deal = new JButton("Deal");
+	
 	public static void main(String[] args) {
 		
 		JFrame f = new JFrame();
@@ -12,19 +19,25 @@ public class GameMain {
 		f.setTitle("Blackjack");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton hitButton   = new JButton("Hit Me");
-	    JButton stickButton = new JButton("Stick");
-	    JButton dealButton  = new JButton("Deal");
+		
 	    
 	    JPanel buttons = new JPanel();
        
-	    buttons.add(hitButton);
-        buttons.add(stickButton);
-        buttons.add(dealButton);
+	    buttons.add(hit);
+        buttons.add(stand);
+        buttons.add(doubleDown);
+        buttons.add(deal);
         
         f.add(buttons);
         
         f.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent b) {
+		if(b.getSource() == hit) {
+			
+		}
+		
 	}
 
 }
