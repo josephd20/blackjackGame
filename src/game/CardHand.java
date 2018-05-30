406ec02177f797c1696ae5abbf491e8036a5017f
Joseph Daher <39558763+josephd20@users.noreply.github.com>
2018-05-30 08:43:40 -0400

    Revert "jeff"
    This reverts commit f0df02a830d6d42180e4542a2eccd7221881107f.

---
+++ src/game/CardHand.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.util.ArrayList;
+
+public abstract class CardHand {
+	
+	ArrayList<Card> cards;
+	
+	abstract Card removeCard();
+	abstract void addCard(Card c);
+	
+	// TEST
+	
+}
