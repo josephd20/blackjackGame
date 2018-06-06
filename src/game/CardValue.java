ef08593a9bc4926fe3bc05bc5bba87611584e730
ben7238 <39558903+ben7238@users.noreply.github.com>
2018-06-06 09:50:59 -0400

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/josephd20/blackjackGame"""
    This reverts commit 62805ca2f1b8df1a7d02edb50e70e876140c5fae.

--- src/game/CardValue.java
+++ src/game/CardValue.java
@@ -1,9 +1,5 @@
 package game;
 
-/**
- * Enumeration Type for the value of a card, which can be one of thirteen values.
- * @author Joseph Daher
- */
 public enum CardValue {
-	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+	ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 }
