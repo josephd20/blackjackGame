9e6b9d92c5d18a820acb0e975879199b9bc0c067
ben7238 <39558903+ben7238@users.noreply.github.com>
2018-06-06 09:45:06 -0400

    Revert "Merge branch 'master' of https://github.com/josephd20/blackjackGame"
    This reverts commit ed5842d3ca012c35c96496521448e42a37142bff, reversing
    changes made to a143663efc7025876f1b411048011ef4953b3a4e.

--- src/game/Card.java
+++ src/game/Card.java
@@ -1,50 +1,29 @@
 package game;
 
-/**
- * Class modeling a playing card, with a suit (heart/spades/diamonds/clubs) and value (1-10/jack/queen/king/ace)
- * @author Joseph Daher
- */
 public class Card {
 
 	private CardSuit suit;
 	private CardValue value;
 	
-	/**
-	 * Constructor for Card requires a designated suit and value.
-	 * @param suit suit of playing card (i.e. diamonds)
-	 * @param value value of playing card (i.e. ace)
-	 */
 	public Card(CardSuit suit, CardValue value)
 	{
 		this.suit = suit;
 		this.value = value;
 	}
 	
-	/**
-	 * Accessor method for suit of a Card object
-	 * @return suit of Card
-	 */
 	public CardSuit getSuit()
 	{
 		return suit;
 	}
 	
-	/**
-	 * Accessor method for value of a Card object
-	 * @return value of Card
-	 */
 	public CardValue getValue()
 	{
 		return value;
 	}
 	
-	/**
-	 * Returns the numerical value corresponding to this card, with Jack, King and Queen being worth 10 and Ace being worth 11
-	 * @return integer corresponding to numerical value of card
-	 */
 	public int getNumericalValue()
 	{
-		int[] values = {2,3,4,5,6,7,8,9,10,10,10,11};
+		int[] values = {1,2,3,4,5,6,7,8,9,10,10,10,11};
 		return values[this.getValue().ordinal()];
 	}
 }
--- src/game/PlayerDeck.java
+++ src/game/PlayerDeck.java
@@ -2,23 +2,14 @@ package game;
 
 import java.util.ArrayList;
 
-/**
- * Subclass of CardHand for all Player card hands, containing an additional method to get the Blackjack value of the hand.
- * @author Joseph Daher
- *
- */
 public class PlayerDeck extends CardHand {
 
-	/**
-	 * Default constructor for PlayerDeck; constructs an empty card hand.
-	 */
 	public PlayerDeck()
 	{
-		cards = new ArrayList<Card>();
+		ArrayList<Card> cards = new ArrayList<Card>();
 	}
 
 	@Override
-<<<<<<< HEAD
 	public Card removeCard() {
 		
 		return null;
@@ -35,29 +26,6 @@ public class PlayerDeck extends CardHand {
 			s+=c.getNumericalValue();
 		}
 		return s;
-=======
-	Card removeCard() {
-		return cards.remove(0);
-	}
-
-	@Override
-	void addCard(Card c) {
-		cards.add(c);
-	}
-	
-	/**
-	 * Returns the numerical Blackjack value of this card hand, as according to typical rules of Blackjack. Aces are worth 11 unless the total is otherwise over 21, in which case Aces are worth 1.
-	 * @return total value of this card hand
-	 */
-	int getValue()
-	{
-		return CardUtil.getValue(cards);
-	}
-	
-	public String toString()
-	{
-		return cards.toString();
->>>>>>> fd50d9cd24a826a34f9d5dc6d7d2b8a9313998d8
 	}
 
 }
