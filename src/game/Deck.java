6cd6e98cd6add9dc0cc9e48ece3d3de592442ada
ben7238 <39558903+ben7238@users.noreply.github.com>
2018-06-06 09:53:43 -0400

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/josephd20/blackjackGame""""
    This reverts commit ef08593a9bc4926fe3bc05bc5bba87611584e730.

---
+++ src/game/Deck.java
@@ -0,0 +1,69 @@
+package game;
+
+import java.util.ArrayList;
+
+/**
+ * Subclass of CardHand to model the dealer deck in Blackjack, consisting of one or more complete deck of cards that are shuffled.
+ * @author Joseph Daher
+ */
+public class Deck extends CardHand {
+
+	/**
+	 * Default constructor for Deck only uses 1 deck.
+	 */
+	public Deck()
+	{
+		cards = new ArrayList<Card>();
+		cards.addAll(CardUtil.createDeck());
+		CardUtil.shuffle(cards);
+	}
+	
+	/**
+	 * Constructor for deck that shuffles a predetermined number of decks together
+	 * @param decks number of card decks used
+	 */
+	public Deck(int decks)
+	{
+		cards = new ArrayList<Card>();
+		for(int i = 0; i < decks; i++)
+		{
+			cards.addAll(CardUtil.createDeck());
+		}
+		CardUtil.shuffle(cards);
+	}
+	
+	@Override
+	void addCard(Card c) {
+		cards.add(c);
+	}
+
+	@Override
+	Card removeCard() {
+		return cards.remove(0);
+	}
+	
+	/**
+	 * Alternative method call for removeCard()
+	 * @return top Card
+	 */
+	Card dealCard() {
+		return this.removeCard();
+	}
+	
+	/**
+	 * Shuffles this deck
+	 */
+	void shuffle() {
+		CardUtil.shuffle(cards);
+	}
+	
+	/**
+	 * Returns this deck as an ArrayList of Cards
+	 * @return ArrayList of Cards in this deck
+	 */
+	ArrayList<Card> getCards()
+	{
+		return cards;
+	}
+	
+}
